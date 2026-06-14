7a69d3e1d5dc6279e0409aa22e195745b54065fe
HuongNguyenQuoc <quochuong05ln@gmail.com>
2026-06-14 16:38:41 +0700

    Finished the video feed and fixed the basic syntax errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import AppNavigation from "@/components/AppNavigation";
+import VideoFeed from "@/components/VideoFeed";
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-black text-white lg:pl-24"> {/* lg:pl-24 adds left padding on large screens */}
       <AppNavigation /> {/* renders the sidebar on desktop and bottom navigation on mobile */}
+      <VideoFeed />
     </div> 
   );
 }
---
+++ src/components/VideoFeed.tsx
@@ -0,0 +1,14 @@
+import VideoCard from "@/components/VideoCart";
+import { videos } from "@/data/videos"
+
+export default function VideoFeed() {
+  return (
+    <main className="h-screen overflow-y-scroll scroll-smooth snap-y snap-mandatory bg-black no-scrollbar">
+      <div className="mx-auto h-full w-full max-w-[480px] lg:max-w-[430px]">
+        {videos.map((video) => {
+          return <VideoCard key={video.id} video={video} />
+        })}
+      </div>
+    </main>
+  );
+}
